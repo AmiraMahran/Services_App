dbebc96273b5553ac036b324e58515fa04fafca5
alaa <alaakhalid72003@gmail.com>
2024-05-02 20:40:51 +0300

    Add Header of Home page

---
+++ Components/HomeComponent/Header.js
@@ -0,0 +1,80 @@
+import { View, Text, Image, StyleSheet, TextInput } from 'react-native'
+import React from 'react'
+import { FontAwesome } from '@expo/vector-icons'
+
+export default function Header() {
+    return (
+        <View style={styles.container}>
+            {/**Profile Section */}
+            <View style={styles.profileMainContainer}>
+                <View style={styles.profileContainer}>
+                    <Image source={require('../../assets/images/avatar.png')} style={styles.userImage} />
+                    <View>
+                        <Text style={{ color: 'white' }}> Welcome </Text>
+                        <Text style={{ color: 'white', fontSize: 20 }}> username </Text>
+                    </View>
+                </View>
+                <FontAwesome name='bookmark-o' size={27} color="white" />
+            </View>
+            {/**Search Bar Section */}
+            <View style={styles.searchBarContainer}>
+                <TextInput
+                placeholder='Search'
+                style={styles.textinput}
+                />
+                <FontAwesome name='search'
+                style={styles.searchBtn}
+                size={27}  />
+
+            </View>
+        </View>
+    )
+}
+const styles = StyleSheet.create({
+    container: {
+        padding: 20,
+        paddingTop: 40,
+        backgroundColor: 'pink',
+        borderBottomLeftRadius: 25,
+        borderBottomRightRadius: 25,
+
+    },
+    profileMainContainer: {
+        display: 'flex',
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent:"space-between",
+
+    },
+    profileContainer: {
+        display: 'flex',
+        flexDirection: "row",
+        alignItems: "center",
+        gap: 10
+    },
+    userImage: {
+        width: 45,
+        height: 45,
+        borderRadius: 99
+    },
+    textinput:{
+       padding:7,
+       paddingHorizontal:16,
+       backgroundColor:'white',
+       borderRadius:8,
+       width:"85%", 
+       fontSize:16
+    },
+    searchBarContainer:{
+        marginTop:15,
+        display:"flex",
+        flexDirection:"row",
+        gap:10,
+        marginBottom:10
+    },
+    searchBtn:{
+       backgroundColor:"white",
+       padding:10,
+       borderRadius:8 
+    }
+})
--- FirebaseConfig.js
+++ FirebaseConfig.js
@@ -1,34 +1,30 @@
-// Import the functions you need from the SDKs you need
-import AsyncStorage from "@react-native-async-storage/async-storage";
+
 import { initializeApp } from "firebase/app";
+
 import { initializeAuth } from "firebase/auth";
-import {getReactNativePersistence} from "@firebase/auth/dist/rn/index.js"
-import { collection, getFirestore } from "firebase/firestore";
+import { getReactNativePersistence } from '@firebase/auth/dist/rn/index.js';
 
-import { onAuthStateChanged } from "firebase/auth";
-
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import {getFirestore,collection} from 'firebase/firestore'
 
 
-// Your web app's Firebase configuration
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
 const firebaseConfig = {
-    apiKey: "AIzaSyCH10BeSYI4-Xi0iZwGs_qi_J6YGUqVYvI",
-    authDomain: "project-a4c36.firebaseapp.com",
-    projectId: "project-a4c36",
-    storageBucket: "project-a4c36.appspot.com",
-    messagingSenderId: "556720487485",
-    appId: "1:556720487485:web:34f33a3a90660e98083e54"
-};
+    apiKey: "AIzaSyCFfGCsnXBokPUKBuRTnVaWkwZwdKrp_X0",
+    authDomain: "homeservice-cab80.firebaseapp.com",
+    projectId: "homeservice-cab80",
+    storageBucket: "homeservice-cab80.appspot.com",
+    messagingSenderId: "837297059373",
+    appId: "1:837297059373:web:50a15ca07f1bc289156aa4"
+  };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-const auth = initializeAuth(app, {
-    persistence: getReactNativePersistence(AsyncStorage),
+export const auth =initializeAuth(app,{
+    projectId:getReactNativePersistence(AsyncStorage)
 });
 
-const db = getFirestore(app);
-const usersRef = collection(db, 'users');
+export const db =getFirestore(app);
 
-export { app, auth, db, usersRef };
+export const usersRef=collection(db,'users');
