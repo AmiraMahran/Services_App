e78ad34b09f1e269ba870c2bc75d743064bb4bb5
alaa <alaakhalid72003@gmail.com>
2024-05-02 20:55:36 +0300

    Edit

--- Components/HomeComponent/Slider.js
+++ Components/HomeComponent/Slider.js
@@ -1,53 +1,78 @@
 import { View, Text, StyleSheet, FlatList, Image } from 'react-native';
 import React, { useEffect, useState } from 'react';
-import { collection, getDocs } from 'firebase/firestore';
+import { collection, getDocs, setDoc, doc  } from 'firebase/firestore';
 import { db } from '../../FirebaseConfig';
 import Heading from './Heading';
-import sliderdata from './Data/SliderData.json'
+import data from './Data/SliderData.json'
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export default function Slider() {
-  const [slider, setSlider] = useState([]);
+    const [slider, setSlider] = useState([]);
 
-  useEffect(() => {
-    getSlider();
-  }, []);
-
-  const getSlider = async () => {
-    try {
-      const snapshot = await getDocs(collection(db, 'alaa'));
-      if (snapshot.empty) {
-        console.log('No data in Firebase collection, loading local data...');
-        setSlider(sliderdata);
-      } else {
-        const sliderData = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
-        setSlider(sliderData);
+    useEffect(() => {
+      getSlider();
+    }, []);
+  
+  
+    const setDataSlider = async (jsonData) => {
+      const collectionRef = collection(db, 'slider');
+      const promises = jsonData.map((item) => {
+        const docRef = doc(collectionRef);
+        return setDoc(docRef, item);
+      });
+    
+      await Promise.all(promises);
+    };
+  
+   
+  
+    const getSlider = async () => {
+      try {
+        const jsonValue = await AsyncStorage.getItem('slider');
+        if (jsonValue !== null) {
+          // We have data!!
+          const sliderData = JSON.parse(jsonValue);
+          setSlider(sliderData);
+        } else {
+         // If there's no data in AsyncStorage, fetch from Firebase
+          const snapshot = await getDocs(collection(db, 'slider'));
+          if (snapshot.empty) {
+            console.log('No data in Firebase collection, setting local data...');
+            await setDataSlider(data); // Set the data in Firebase
+            setSlider(data); // Update the local state
+            await AsyncStorage.setItem('slider', JSON.stringify(data));
+          } else {
+            const sliderData = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+            setSlider(sliderData);
+            await AsyncStorage.setItem('slider', JSON.stringify(sliderData));
+          }
+         }
+      } catch (error) {
+        console.error('Error getting slider data:', error);
       }
-    } catch (error) {
-      console.error('Error getting slider data:', error);
-    }
-  };
-
-  return (
-    <View>
-      <Heading text={'Offers For You'} />
-      <FlatList
-        data={slider}
-        horizontal={true}
-        showsHorizontalScrollIndicator={false}
-       
-        renderItem={({ item }) => (
-          <View style={{marginRight:20}}>
-            <Image
-              source={{ uri: item.image }}
-              style={styles.sliderImage}
-            
-            />
-          </View>
-        )}
-      />
-    </View>
-  );
-}
+    };
+  
+    return (
+      <View>
+        <Heading text={'Offers For You'} />
+  
+        <FlatList
+          data={slider}
+          horizontal={true}
+          showsHorizontalScrollIndicator={false}
+          renderItem={({ item }) => (
+            <View style={{ marginRight: 20 }}>
+              <Image
+                source={{ uri: item.image }}
+                style={styles.sliderImage}
+  
+              />
+            </View>
+          )}
+        />
+      </View>
+    );
+  }
 
 const styles = StyleSheet.create({
  
