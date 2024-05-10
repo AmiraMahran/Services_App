a8c935844b77793640258cf26166ac9e974f5b13
Mariam-MohamedM <mariam1235vs@gmail.com>
2024-05-10 21:12:59 +0300

    add some additional info

--- Components/HomeComponent/Footer.js
+++ Components/HomeComponent/Footer.js
@@ -4,20 +4,40 @@ import { FontAwesome } from '@expo/vector-icons'
 import { useAuth } from '../../firebase/auth'
 
 export default function Footer() {
-    const {user}= useAuth();
+    const { user } = useAuth();
     return (
+        // main fotter 
         <View style={styles.container}>
-           <View style={styles.box}>
-            <Text style={styles.txt}>for more .. contact us</Text>
-            <Text
-             style={styles.txt}
-             onPress={()=>{Linking.openURL('mailto:'+user?.email+'?subject=I am looking for your service&body=Hi There,')}}
-             
-            >
-                servicesApp@gmail.com
-                </Text>
 
-           </View>
+            {/* first one */}
+            <View style={styles.subcontainer1}>
+                <Text style={{ color: '#003c43' }}>
+                    Find out what makes us the #1 email marketing and automations platform
+                </Text>
+                <Text style={{ color: '#003c43' }}>
+                    Start your free website trial today
+                </Text>
+            </View>
+
+            {/* second one */}
+            <View style={styles.subcontainer1}>
+
+                <View style={styles.box}>
+                    <Text style={styles.txt}>for more .. contact us</Text>
+                    <Text
+                        style={{ color: '#135D66' }}
+                        onPress={() => { Linking.openURL('mailto:' + user?.email + '?subject=I am looking for your service&body=Hi There,') }}
+                    >
+                        servicesApp@gmail.com
+                    </Text>
+                </View>
+
+                <View style={styles.box}>
+                    <Text style={styles.txt}>Our app is ready to help you </Text>
+                </View>
+                
+            </View>
+
         </View>
     )
 }
@@ -30,11 +50,20 @@ const styles = StyleSheet.create({
         borderBottomRightRadius: 25,
 
     },
-    box:{
-        padding:20,
+    subcontainer1: {
+        padding: 20,
+        paddingTop: 40,
+        backgroundColor: '#77B0AA',
+        borderBottomLeftRadius: 25,
+        borderBottomRightRadius: 25,
+    },
+    box: {
+        padding: 20,
+        marginBottom: 20,
+
 
     },
-    txt:{
-        color:'white'
+    txt: {
+        color: 'white'
     },
 })
